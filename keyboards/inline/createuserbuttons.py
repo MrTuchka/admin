e3dbf21beb81ec8181e5a8301b1ca95387eefc1a
Nafees Anwar <h.nafees.anwar@gmail.com>
2022-08-29 13:36:19 +0500

    Added bot deployer. (#1)
    Co-authored-by: Nafees Anwar <h.nafees.anwar@gamil.com>

--- keyboards/inline/createuserbuttons.py
+++ keyboards/inline/createuserbuttons.py
@@ -1,11 +1,22 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-createuser = InlineKeyboardMarkup(row_width=2,
-                                 inline_keyboard=[
-                                     [
-                                         InlineKeyboardButton(
-                                             text="Create",
-                                             callback_data="create"
-                                         ),
-                                     ]
-                                 ])
+# you can configure more bot types here.
+BOT_TEMPLATES = [
+    {
+        "button_text": "Create Support Bot",
+        "bot_id": "support_bot",
+        "github_repo": "git@github.com:MrTuchka/support-bot.git",
+    }
+]
+
+createuser = InlineKeyboardMarkup(
+    row_width=2,
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text=bot_template["button_text"],
+                callback_data=f"create_bot__{bot_template['bot_id']}",
+            ) for bot_template in BOT_TEMPLATES
+        ]
+    ]
+)
